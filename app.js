08459fe995a95c0a6daee8eb4fc180c18e9a29c0
travoemily-fs <EATravo@student.fullsail.edu>
2026-01-22 12:30:31 -0800

    hard coded secret management style added

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-require("dotenv").config("./.env")
+require("dotenv").config("./.env");
 
-console.log(process.env.NODE_ENV)
+// hard-coded secret
+const API_KEY = "super secret key 1234";
+
+console.log(process.env.NODE_ENV);
 
 const http = require("http");
-const hostname = "127.0.0.1"
+const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  const fs = require("fs");
+  let directory_name = "./";
+  let filenames = fs.readdirSync(directory_name);
 
-    const fs = require("fs")
-    let directory_name = "./"
-    let filenames = fs.readdirSync(directory_name);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
 
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain")
+  let f = "";
+  filenames.forEach((file) => {
+    f += file + "\n";
+  });
 
-    f = ""
-    filenames.forEach((file)= {
-        f : f +file+" \n"
-    })
-res.end(f)
+  res.end(f + "my secret key is: " + API_KEY);
 });
 
 server.listen(port, hostname, () => {
-    
-})
+  console.log(`server is running on ${port}!`);
+});
